b145fe6b4df5d3bb4c46f3b4b6184667e4cce084
raihanM95 <mahmud.raihan@yahoo.com>
2021-09-21 12:38:04 +0600

    added IServices

---
+++ Building.BLL/IServices/IDropdownLoadService.cs
@@ -0,0 +1,16 @@
+﻿using Entity = Building.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building.BLL.IServices
+{
+    public interface IDropdownLoadService
+    {
+        Task<IReadOnlyList<Entity.Building>> GetBuildingDropdownDataAsync();
+        Task<IReadOnlyList<Entity.DataField>> GetDataFieldDropdownDataAsync();
+        Task<IReadOnlyList<Entity.Object>> GetObjectDropdownDataAsync();
+    }
+}
---
+++ Building.BLL/IServices/IReadingService.cs
@@ -0,0 +1,16 @@
+﻿using Building.Domain.DTOs;
+using Building.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building.BLL.IServices
+{
+    public interface IReadingService
+    {
+        Task<IReadOnlyList<Reading>> GetAllAsync();
+        Task<IReadOnlyList<Reading>> GetAllFilteredAsync(ReadingDto readingDto);
+    }
+}
