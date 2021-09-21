97d10ac22a1a9a8fa754980ca46658270979043d
raihanM95 <mahmud.raihan@yahoo.com>
2021-09-21 13:56:52 +0600

    create BuildingController

---
+++ Building.API/Controllers/BuildingController.cs
@@ -0,0 +1,30 @@
+﻿using Building.BLL.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Building.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuildingController : ControllerBase
+    {
+        private readonly IDropdownLoadService _dropdownLoadService;
+
+        public BuildingController(IDropdownLoadService dropdownLoadService)
+        {
+            _dropdownLoadService = dropdownLoadService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _dropdownLoadService.GetBuildingDropdownDataAsync();
+
+            return Ok(data);
+        }
+    }
+}
