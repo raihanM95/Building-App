9947fe6c9a747a12daa247a026246daaa242b83b
raihanM95 <mahmud.raihan@yahoo.com>
2021-09-21 13:59:25 +0600

    create ObjectController

---
+++ Building.API/Controllers/ObjectController.cs
@@ -0,0 +1,30 @@
+﻿using Building.BLL.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Building.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ObjectController : ControllerBase
+    {
+        private readonly IDropdownLoadService _dropdownLoadService;
+
+        public ObjectController(IDropdownLoadService dropdownLoadService)
+        {
+            _dropdownLoadService = dropdownLoadService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _dropdownLoadService.GetObjectDropdownDataAsync();
+
+            return Ok(data);
+        }
+    }
+}
