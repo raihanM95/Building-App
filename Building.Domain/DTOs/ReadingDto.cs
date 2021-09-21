7f3309630eb9f7c5d99100936708ab22113667e4
raihanM95 <mahmud.raihan@yahoo.com>
2021-09-21 11:11:35 +0600

    added ReadingDto

---
+++ Building.Domain/DTOs/ReadingDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building.Domain.DTOs
+{
+    public class ReadingDto
+    {
+        public int BuildingId { get; set; }
+        public int DataFieldId { get; set; }
+        public int ObjectId { get; set; }
+        public DateTime TimestampFrom { get; set; }
+        public DateTime TimestampTo { get; set; }
+    }
+}
