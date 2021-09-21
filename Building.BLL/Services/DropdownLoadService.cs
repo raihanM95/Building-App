cb7c51caaae2b297c050f6b747c7fad479e365e0
raihanM95 <mahmud.raihan@yahoo.com>
2021-09-21 12:52:44 +0600

    added Services

---
+++ Building.BLL/Services/DropdownLoadService.cs
@@ -0,0 +1,36 @@
+﻿using Building.BLL.IServices;
+using Building.DAL;
+using Building.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building.BLL.Services
+{
+    public class DropdownLoadService : IDropdownLoadService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DropdownLoadService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<IReadOnlyList<Domain.Entities.Building>> GetBuildingDropdownDataAsync()
+        {
+            return _unitOfWork.BuildingRepository.GetAllAsync();
+        }
+
+        public Task<IReadOnlyList<DataField>> GetDataFieldDropdownDataAsync()
+        {
+            return _unitOfWork.DataFieldRepository.GetAllAsync();
+        }
+
+        public Task<IReadOnlyList<Domain.Entities.Object>> GetObjectDropdownDataAsync()
+        {
+            return _unitOfWork.ObjectRepository.GetAllAsync();
+        }
+    }
+}
---
+++ Building.BLL/Services/ReadingService.cs
@@ -0,0 +1,32 @@
+﻿using Building.BLL.IServices;
+using Building.DAL;
+using Building.Domain.DTOs;
+using Building.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building.BLL.Services
+{
+    public class ReadingService : IReadingService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ReadingService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<IReadOnlyList<Reading>> GetAllAsync()
+        {
+            return _unitOfWork.ReadingRepository.GetAllAsync();
+        }
+
+        public Task<IReadOnlyList<Reading>> GetAllFilteredAsync(ReadingDto readingDto)
+        {
+            return _unitOfWork.ReadingRepository.GetAllFilteredAsync(readingDto);
+        }
+    }
+}
